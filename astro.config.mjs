f37119544eb8b41ba33fcc10c59628f9a0bc2c17
victorzarzar <victorzarzar58@gmail.com>
2026-07-13 13:18:45 -0300

    feat: add new components and netifly && vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,32 @@
 
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	i18n: {
-		locales: ["es", "en", "pt-br"],
-		defaultLocale: "en",
-	},
+  i18n: {
+    locales: ["es", "en", "pt-br"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 
-	integrations: [
-		sitemap(),
-		compress({
-			css: false,
-			html: true,
-			js: true,
-			img: {
-				quality: 80,
-			},
-			svg: true,
-			logger: 1,
-		}),
-		preact({
-			compat: true,
-		}),
-	],
+  integrations: [
+    sitemap(),
+    compress({
+      css: false,
+      html: true,
+      js: true,
+      img: {
+        quality: 80,
+      },
+      svg: true,
+      logger: 1,
+    }),
+    preact({
+      compat: true,
+    }),
+  ],
 });
